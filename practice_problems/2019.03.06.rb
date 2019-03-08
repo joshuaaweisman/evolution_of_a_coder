55c5447b52982dfbeaf130de0c086e0d28e90b09
joshua.a.weisman@gmail.com <joshua.a.weisman@gmail.com>
2019-03-07 20:49:34 -0800

    more practice problems

--- practice_problems/2019.03.06.rb
+++ practice_problems/2019.03.06.rb
@@ -4,6 +4,61 @@ require "byebug"
 #####################################################################
 # independent methods
 #####################################################################
+# Leetcode #200
+# Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+                                            # grid = [
+                                            #     [1, 1, 1, 1, 0],
+                                            #     [1, 1, 0, 1, 0],
+                                            #     [1, 1, 0, 0, 0],
+                                            #     [0, 0, 0, 0, 0]
+                                            # ]
+
+                                            # def check_neighbors(current_position, used_positions, grid)
+                                            #     row = current_position[0]
+                                            #     col = current_position[1]
+                                                
+                                            #     up_neighbor = (row > 0) ? [row - 1, col] : nil
+                                            #     down_neighbor = (row < 3) ? [row + 1, col] : nil
+                                            #     left_neighbor = (col > 0) ? [row, col - 1] : nil
+                                            #     right_neighbor = (col < 4) ? [row, col + 1] : nil
+                                            #     neighbors = [up_neighbor, down_neighbor, left_neighbor, right_neighbor]
+                                                
+                                            #     neighbors.each do |neighbor|
+                                            #         tile = grid[neighbor[0], neighbor[1]]
+                                            #         if neighbor != nil && tile == 1 && !used_positions.include?(neighbor)
+                                            #             used_positions << neighbor
+                                            #             check_neighbors(neighbor)
+                                            #         else
+                                            #             used_positions << neighbor
+                                            #         end
+                                            #     end
+                                            # end
+
+
+                                            # def num_islands(grid)
+                                            #     count = 0
+                                            #     used_positions = []
+                                                
+                                            #     grid.each_with_index do |row, idxr|
+                                            #         grid.each_with_index do |col, idxc|
+                                                        
+                                            #             current_position = [idxr, idxc]     
+                                            #             used_positions << current_position
+                                                        
+                                            #             if current_position == 1 && !used_positions.include?(current_position)
+                                            #                 used_positions += check_neighbors(current_position, used_positions, grid)
+                                            #                 count += 1
+                                            #             end
+                                                    
+                                            #         end
+                                            #     end
+                                                
+                                            #     count
+                                            # end
+
+
+
 # Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple 
 # of both of the given numbers
 def least_common_multiple(num_1, num_2)
@@ -63,7 +118,28 @@ end
 
 
 
+# Write a recursive method that takes in a string to search and a key string.
+# Return true if the string contains all of the characters in the key
+# in the same order that they appear in the key.
+#
+# string_include_key?("cadbpc", "abc") => true
+# string_include_key("cba", "abc") => false
+def string_include_key?(string, key)
+    return true if key.length == 0
+
+    target_char = key[0]
+    string.each_char.with_index do |char, idx|
+        if char == target_char
+            return string_include_key?(string[idx + 1..-1], key[1..-1])
+        end
+    end
+    false
+end
+
+
+
 def subsets(array)
+
     return [array] if array.length == 1
 
     combos = []
@@ -112,34 +188,23 @@ end
 # array class
 #####################################################################
 class Array
-    # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
-    def pair_sum_count(num)
-        count = 0
-        self.each_with_index do |num1, idx|
-            self[idx + 1..-1].each do |num2|
-                count += 1 if num1 + num2 == num
-            end
-        end
-        count
-    end
-
-
-
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
     # When given a proc, the method should sort the array according to the proc.
     # When no proc is given, the method should sort the array in increasing order.
-    def bubble_sort(&prc)  
+    def bubble_sort(&prc)
         prc ||= Proc.new {|a, b| a <=> b}
-
         sorted = false
-        while sorted == false
-            sorted = true
-            self.each_with_index do |num1, idx|
-                next if idx == self.length - 1
-                num2 = self[idx + 1]
 
-                if prc.call(num1, num2) == 1
-                    self[idx], self[idx + 1] = self[idx + 1], self[idx]
+        while !sorted
+            sorted = true
+
+            self.each_with_index do |el1, idx1|
+                next if idx1 > self.length - 2
+                idx2 = idx1 + 1
+                el2 = self[idx2]
+
+                if prc.call(el1, el2) > 0
+                    self[idx1], self[idx2] = self[idx2], self[idx1]
                     sorted = false
                 end
             end
@@ -153,6 +218,19 @@ class Array
     # my_all
     def my_all?(&prc)
         self.each {|el| return false if prc.call(el) == false}
+    end
+
+
+
+    # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
+    def pair_sum_count(num)
+        count = 0
+        self.each_with_index do |num1, idx|
+            self[idx + 1..-1].each do |num2|
+                count += 1 if num1 + num2 == num
+            end
+        end
+        count
     end
 
 end
@@ -170,6 +248,24 @@ class Hash
             inverted[val] = key
         end
         inverted
+    end
+
+
+
+    # Write a version of merge. This should NOT modify the original hash
+    def my_merge(hash2)
+        merged = Hash.new
+        self.each {|k, v| merged[k] = v}
+        
+        hash2.each do |k, v|
+            if merged[k] == nil
+                merged[k] = v
+            else
+                merged[k] += v
+            end
+        end
+
+        merged
     end
 end
 
