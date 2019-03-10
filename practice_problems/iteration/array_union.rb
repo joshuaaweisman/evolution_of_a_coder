f2035b8f67ba563cb4809cd40570ab1651b385f6
joshua.a.weisman@gmail.com <joshua.a.weisman@gmail.com>
2019-03-10 13:48:35 -0700

    new practice problems

--- practice_problems/iteration/array_union.rb
+++ practice_problems/iteration/array_union.rb
@@ -1,5 +1,3 @@
-require "byebug"
-
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
 
@@ -9,13 +7,13 @@ require "byebug"
 # iterate through all arrays
     # for each array, iterate through AND SHOVEL element into final array
 
-def union(*arrays)
-    final_els = []
-    arrays.each do |arr|
-        arr.each {|el| final_els << el}
+    def union(*arrays)
+        final_els = []
+        arrays.each do |arr|
+            arr.each {|el| final_els << el}
+        end
+        final_els
     end
-    final_els
-end
-
-p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
-p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
+    
+    p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
+    p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
---
+++ practice_problems/iteration/integer_coprime?.rb
@@ -0,0 +1,15 @@
+# Write a method, coprime?(num_1, num_2), that accepts two numbers as args.
+# The method should return true if the only common divisor between the two numbers is 1.
+# The method should return false otherwise. For example coprime?(25, 12) is true because
+# 1 is the only number that divides both 25 and 12.
+
+def coprime?(num_1, num_2)
+    num_1 < num_2 ? counter = num_1 : counter = num_2
+    counter.downto(2).to_a.each {|divisor| return false if num_1 % divisor == 0 && num_2 % divisor == 0}
+    true
+end
+
+p coprime?(25, 12)    # => true
+p coprime?(7, 11)     # => true
+p coprime?(30, 9)     # => false
+p coprime?(6, 24)     # => false
