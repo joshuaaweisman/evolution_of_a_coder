349e2a3fe7c2663e236ff029af74d05ef0b648c6
joshua.a.weisman@gmail.com <joshua.a.weisman@gmail.com>
2019-03-10 23:01:52 -0700

    new practice problems

--- practice_problems/iteration/string_most_frequent_bigram_02.rb
+++ practice_problems/iteration/string_most_frequent_bigram_02.rb
@@ -1,15 +1,12 @@
-require "byebug"
-
 # Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
 # most in the string.
 
 def most_frequent_bigram(str)
     bigram_counts = Hash.new(0)
-    str.chars.each_with_index do |char1, idx1|
-        next if idx1 > str.length - 2
+    str.chars.each_with_index do |char1, idx|
+        next if idx > str.length - 2
 
-        idx2 = idx1 + 1
-        char2 = str[idx2]
+        char2 = str[idx + 1]
         bigram = char1 + char2
         bigram_counts[bigram] += 1
     end
