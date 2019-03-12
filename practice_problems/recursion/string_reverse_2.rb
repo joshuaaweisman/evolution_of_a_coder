7db6ff49aa394993cbec207570298c634c5ec620
joshua.a.weisman@gmail.com <joshua.a.weisman@gmail.com>
2019-03-11 23:55:44 -0700

    update practice problems // string_reverse_2

---
+++ practice_problems/recursion/string_reverse_2.rb
@@ -0,0 +1,16 @@
+# Write a method, reverse_string(str), that takes in a string.
+# The method should return the string with it's characters in reverse order.
+#
+# Solve this recursively!
+#
+# Examples:
+# 
+# reverse_string("")            # => ""
+# reverse_string("c")           # => "c"
+# reverse_string("internet")    # => "tenretni"
+# reverse_string("friends")     # => "sdneirf"
+
+def reverse_string(str)
+    return "" if str.length == 0
+    str[-1] + reverse_string(str[0...-1])
+end
